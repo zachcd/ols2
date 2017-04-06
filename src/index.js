bc07a46c0fca676f7217546ef987b872c7dd91e4
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2017-04-06 09:25:59 -0400

    redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import olsApp from './reducers'
 import './index.css';
 
+let store = createStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );

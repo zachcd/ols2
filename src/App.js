0ec950a6a577cd77580bbbd1a0fc4d49fda62cfa
Zachary Chay-Dolan <Zacharia.cd@gmail.com>
2017-04-25 21:07:20 -0400

    home folder and some home stuff added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import MainHeader from './navigation/mainHeader.js';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import './App.css';
 
 

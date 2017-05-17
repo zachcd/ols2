2ba7d7bb31ed9ad46e46aaa3323a555f4e23c63a
Connor Lewellyn <lewellync@gmail.com>
2017-05-16 21:38:48 -0400

    Beginning of koa-router usage. Added babel-polyfill and transform-regenerator

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,20 @@
 import * as http from 'http'
 import koa from 'koa'
+import api from 'koa-router'
 import logger from 'koa-logger'
+import body from 'koa-body'
 import co from 'co'
 
+const port = (process.env.PORT || 3200)
+
 const app = new koa()
+const router = new api()
+
+router.get('/api', ctx => {
+  ctx.body = "You've reached /api!"
+})
 
 app.use(logger())
-
-
-
-
-app.listen( process.env.PORT || 3200)
+app.use(router.routes())
+app.listen(port)
+console.log('The magic happens on port ' + port)

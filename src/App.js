2b545cfc792ff6c50d736cbe7ed62fb3c45ad69a
Zachary Chay-Dolan <zacharia.cd@gmail.com>
2017-07-25 16:16:39 -0400

    oops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom'
 
 
 //if Organization is opened
@@ -10,11 +10,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Link>Main </Link>
-      <Link>Cards </Link>
-        {Organization()}
-      </div>
+      <BrowserRouter>
+        <div className="App">
+        </div>
+      </BrowserRouter>
     );
   }
 }
